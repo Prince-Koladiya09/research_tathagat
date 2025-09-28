98f564a268bf1e2010987f6ff07cd6a8ff3fdd91
Meet Vyas <meetrvyas7@gmail.com>
2025-09-28 21:55:57 +0530

    added fine tuning methods to cnn

--- cnn_base/Models/CNN/model.py
+++ cnn_base/Models/CNN/model.py
@@ -42,6 +42,15 @@ class Model(Base_Model):
         self._set_trainable_status(self.base_model.layers, trainable=True)
         return self
 
+    def freeze_early_n(self, n: int = None) -> 'Model':
+        if n is None:
+            n = self.config.model.n_layers_to_tune
+        if n > len(self.base_model.layers):
+             self.logger.warning(f"Requested to freeze {n} layers, but model only has {len(self.base_model.layers)}. Freezing all.")
+             n = len(self.base_model.layers)
+        self._set_trainable_status(self.base_model.layers[:n], trainable=False)
+        return self
+
     def unfreeze_later_n(self, n: int = None) -> 'Model':
         if n is None:
             n = self.config.model.n_layers_to_tune
@@ -67,7 +76,28 @@ class Model(Base_Model):
             self.logger.error(f"Error during freeze_upto_layer: {e}")
         return self
     
+    def unfreeze_after_layer(self, layer_name: str) -> 'Model':
+        try:
+            layer_found = False
+            for layer in self.base_model.layers:
+                if layer.name == layer_name:
+                    layer_found = True
+                layer.trainable = layer_found
+            if layer_found:
+                 self.logger.info(f"Froze layers up to '{layer_name}' and unfroze all after and including it.")
+            else:
+                 self.logger.error(f"Layer '{layer_name}' not found in the model. Froze all")
+        except Exception as e:
+            self.logger.error(f"Error during unfreeze_after_layer : {e}")
+        return self
+    
     def add_custom_layers(self, layers_list: list = None) -> 'Model':
+        """
+        layers : list of keras.layers.Layer
+                    If given, then adds these layer after layer_name
+                    If not given, adds custom layers from config
+                    after adding these, adds the remaining layers from the model
+        """
         try:
             if layers_list is None:
                 layers_list = get_custom_layers(self.config.model.num_classes)
@@ -83,13 +113,42 @@ class Model(Base_Model):
             self.logger.error(f"Error adding custom layers: {e}")
         return self
 
+    def add_layers_in_between(self, layer_name : str, layers : list[keras.layers.Layer]) -> 'Model' :
+        """
+        layers : list of keras.layers.Layer
+                    If given, then adds these layer after layer_name
+                    If not given, adds custom layers from config
+                    after adding these, adds the remaining layers from the model
+        """
+        try :
+            if layers_list is None:
+                layers_list = get_custom_layers(self.config.model.num_classes)
+            
+            target_layer = self.model.get_layer(layer_name)
+            index = self.model.layers.index(target_layer)
+
+            x = target_layer.output
+
+            for layer in layers_list :
+                x = layer(x)
+
+            for layer in self.model.layers[index + 1 :] :
+                x = layer(x)
+
+            self.outputs_layer = x
+
+            self._rebuild_model()
+            self.logger.info(f"Added custom layers after {layer_name}")
+        except Exception as e :
+            self.logger.error(e)
+        return self
+
     def cut_at_layer_and_add_custom_layers(self, layer_name: str, layers_list: list = None) -> 'Model':
         try:
             if layers_list is None:
                 layers_list = get_custom_layers(self.config.model.num_classes)
             
-            target_output = self.model.get_layer(layer_name).output
-            x = target_output
+            x = self.model.get_layer(layer_name).output
             for layer in layers_list:
                 x = layer(x)
             self.outputs_layer = x
@@ -98,4 +157,59 @@ class Model(Base_Model):
             self.logger.info(f"Cut model at '{layer_name}' and added {len(layers_list)} custom layers.")
         except Exception as e:
             self.logger.error(f"Error cutting and adding layers at '{layer_name}': {e}")
+        return self
+
+    def cut_at_layer(self, layer_name: str) -> 'Model':
+        try:
+            self.outputs_layer = self.model.get_layer(layer_name).output
+            
+            self._rebuild_model()
+            self.logger.info(f"Cut model at '{layer_name}'.")
+        except Exception as e:
+            self.logger.error(f"Error cutting at '{layer_name}': {e}")
+        return self
+
+    def remove_last_layer(self) -> 'Model' :
+        try :
+            last_second_layer = self.model.layers[-2]
+            self.outputs_layer = last_second_layer.output
+            self._rebuild_model()
+            self.logger.info("Removed last layer")
+        except Exception as e :
+            self.logger.error(e)
+        return self
+
+    def remove_N_layers(self, n : int = None) -> 'Model' :
+        """
+        If you do not provide N, it takes default from config
+        """
+        try :
+            if n is None:
+                n = self.config.model.n_layers_to_tune
+            if n > len(self.base_model.layers):
+                self.logger.warning(f"Requested to remove {n} layers, but model only has {len(self.base_model.layers)}. Removing none.")
+                return self
+            
+            remove_after_layer = self.model.layers[-(n + 1)].name
+            self.outputs_layer = self.model.get_layer(remove_after_layer).output
+            self.logger.info(f"Removed last {n} layers")
+        except Exception as e :
+            self.logger.error(e)
+        return self
+
+    def remove_layer_in_between(self, layer_name : str) -> 'Model' :
+        try :
+            target_layer = self.model.get_layer(layer_name)
+            index = self.model.layers.index(target_layer)
+            prev_layer = self.model.layers[index - 1]
+
+            x = prev_layer.output
+            for layer in self.model.layers[index + 1 :]:
+                x = layer(x)
+            self.outputs_layer = x
+
+            self._rebuild_model()
+            self.logger.info(f"Removed {layer_name} layer and reconnected network")
+        except Exception as e :
+            self.logger.error(e)
         return self
